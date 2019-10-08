be064be84a7eb758b56a27d9144ce37c0be29c17
16meyrat <louis.meyrat@mines-paristech.fr>
2019-10-08 13:46:29 +0200

    Ude sdl as gfx framework

--- src/gui.rs
+++ src/gui.rs
@@ -1,39 +1,91 @@
-use nannou::prelude::*;
+use sdl2::pixels::Color;
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+use sdl2::pixels::PixelFormatEnum;
+use std::time::Duration;
+use std::sync::{mpsc,Arc, Mutex};
 
-enum Key {
+pub const WIDTH: usize = 160;
+pub const HEIGHT: usize = 144;
+pub const DEPTH: usize = 3;
+pub const SIZE: usize = WIDTH * HEIGHT * DEPTH;
+
+enum GBKey {
     Up, Down, Left,Right, A, B, Start, Select
 }
 
 pub enum Message {
     WindowClosed,
-    KeyUp(Key),
-    KeyDown(Key)
+    KeyUp(GBKey),
+    KeyDown(GBKey)
 }
 
-pub fn run_gui() {
-    nannou::app(model).run();
-} 
-
-struct Model {
-    window: WindowId,
+pub struct Gui{
+    context: sdl2::Sdl,
+    canvas: sdl2::render::Canvas<sdl2::video::Window>,
+    target: Arc<Mutex<[u8; SIZE]>>,
+    tx: mpsc::Sender<Message>,
 }
 
-fn model(app: &App) -> Model {
-    // Create a new window! Store the ID so we can refer to it later.
-    let window = app
-        .new_window()
-        .with_dimensions(512, 512)
-        .with_title("YAGBEUM")
-        .view(view) // The function that will be called for presenting graphics to a frame.
-        .event(event) // The function that will be called when the window receives events.
-        .build()
-        .unwrap();
-    Model { window }
+impl Gui{
+    pub fn new(tx: mpsc::Sender<Message>) -> Self {
+        let sdl_context = sdl2::init().unwrap();
+        let video_subsystem = sdl_context.video().unwrap();
+    
+        let window = video_subsystem.window("rust-sdl2 demo", 800, 600)
+            .position_centered()
+            .build()
+            .unwrap();
+    
+        let mut canvas = window.into_canvas().build().unwrap();
+    
+        canvas.clear();
+        canvas.present();
+        
+        Gui {
+            context: sdl_context,
+            canvas,
+            target: Arc::new(Mutex::new([0; SIZE])),
+            tx
+        }
+    }
+    pub fn run(&mut self){
+        let mut event_pump = self.context.event_pump().unwrap();
+
+        let texture_creator = self.canvas.texture_creator();
+        let mut texture = texture_creator.create_texture_static(
+            PixelFormatEnum::RGB24,
+            WIDTH as u32,
+            HEIGHT as u32
+        ).expect("Could not allocate texture");
+
+        'running: loop {
+            {
+                let target = self.target.lock().expect("Mutex error");
+                texture.update(None, &target[..], WIDTH * DEPTH).expect("Could not update texture");
+            }
+            self.canvas.copy(&texture, None, None).expect("Could not render texture");
+            
+            for event in event_pump.poll_iter() {
+                match event {
+                    Event::Quit {..} |
+                    Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
+                        break 'running
+                    },
+                    _ => {}
+                }
+            }
+            // The rest of the game loop goes here...
+            self.canvas.present();
+            ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
+        }
+
+        self.tx.send(Message::WindowClosed).unwrap();
+    } 
+
+    pub fn get_texture(&self) -> Arc<Mutex<[u8; SIZE]>> {
+        self.target.clone()
+    }
 }
 
-fn event(_app: &App, _model: &mut Model, _event: WindowEvent) {
-}
 
-fn view(_app: &App, _model: &Model, frame: &Frame) {
-    frame.clear(CORNFLOWERBLUE);
-}
--- src/main.rs
+++ src/main.rs
@@ -5,25 +5,53 @@ use gbc::bus::Bus;
 use gbc::cpu::Cpu;
 use gbc::cartridge::load_rom;
 
+use gui::{Message, Gui};
+
 use std::thread;
-use std::sync::mpsc;
+use std::sync::{mpsc, Arc, Mutex};
 
 extern crate num_enum;
-extern crate nannou;
+extern crate sdl2;
 
 fn main() {
-    let (rx, tx) = mpsc::channel::<gui::Message>();
-    gui::run_gui();
+    let (tx, rx) = mpsc::channel::<gui::Message>();
+    let mut gui = Gui::new(tx);
+    let texture = gui.get_texture();
+    let emu_thread = thread::spawn(move || {
+        run_emulator(rx, texture);
+    });
+    gui.run();
+    emu_thread.join().unwrap();
 }
 
-fn run_emulator() {
+fn run_emulator(rx: mpsc::Receiver::<gui::Message>, texture: Arc<Mutex<[u8; gui::SIZE]>>) {
     let mut rom = load_rom("Tetris.GB");
     let mut bus = Bus::new(&mut *rom);
     let mut cpu = Cpu::new(& mut bus);
 
     cpu.reset();
+
+    {
+        let mut tex = texture.lock().expect("Mutex error");
+        for (i, byte) in tex.iter_mut().enumerate() {
+            if i % 3 == 0 {
+                *byte = 255;
+            }
+        }
+    }
     
     loop {
         cpu.tick();
+
+        match rx.try_recv() {
+            Ok(event) => {
+                match event {
+                    Message::WindowClosed => break,
+                    _ => {}
+                }
+            }
+            Err(mpsc::TryRecvError::Disconnected) => break,
+            Err(mpsc::TryRecvError::Empty) => {}
+        }
     }
 }
