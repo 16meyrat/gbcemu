fb5c27f44b34f6b0a21516f63b47650687b547df
16meyrat <louis.meyrat@gmail.com>
2023-02-19 18:19:27 +0100

    Implement MBC3

--- src/gbc/cartridge.rs
+++ src/gbc/cartridge.rs
@@ -1,10 +1,11 @@
 use anyhow::{bail, Context, Result};
 use num_enum::TryFromPrimitive;
+use time::{OffsetDateTime, Duration};
 use std::convert::TryFrom;
 use std::fs::{File, OpenOptions};
 use std::io::{prelude::*, SeekFrom};
 use std::path::PathBuf;
-use std::str;
+use std::{str, slice};
 
 pub trait Cartridge {
     fn read(&self, addr: u16) -> u8;
@@ -45,12 +46,11 @@ pub fn load_rom(path: &str) -> Result<Box<dyn Cartridge>> {
             )?)
         }
         MbcType::Mbc3 | MbcType::Mbc3Ram | MbcType::Mbc3RamBattery => {
-            bail!("Unimplemented MBC3")
-            // println!("Mapper is MBC3");
-            // Box::new(MBC1::new(
-            //     &mut rom,
-            //     matches!(mbc_type, MbcType::Mbc3RamBattery),
-            // )?)
+            println!("Mapper is MBC3");
+            Box::new(MBC3::new(
+                &mut rom,
+                matches!(mbc_type, MbcType::Mbc3RamBattery),
+            )?)
         }
         _ => {
             bail!("Unsuported MBC : {mbc_type:?}");
@@ -320,13 +320,8 @@ impl MBC2 {
             ram_enable: false,
             bank_selection: 1,
         };
-        let mut i = 0;
-        for chunk in rom_data.chunks(0x4000) {
+        for (i, chunk) in rom_data.chunks(0x4000).enumerate() {
             res.banks[i][..chunk.len()].copy_from_slice(chunk);
-            i += 1;
-            if i & 0x1f == 0 {
-                i += 1;
-            }
         }
         Ok(res)
     }
@@ -380,6 +375,242 @@ impl Cartridge for MBC2 {
                     self.bank_selection = if masked > 0 {masked} else {1};
                 } else {
                     self.ram_enable = val & 0x0f == 0x0a;
+                }
+            }
+            _ => {
+                panic!("Illegal cartridge write at {addr:#x}")
+            }
+        };
+    }
+}
+
+struct MBC3 {
+    banks: Vec<[u8; 0x4000]>,
+    ram: Vec<[u8; 0x2000]>,
+    save_file: Option<File>,
+    ram_enable: bool,
+    rom_selection: u8,
+    ram_is_rtc: bool,
+    ram_rtc_selection: u8,
+    time_offset: Duration,
+    latched_time: Option<OffsetDateTime>,
+    time_stopped: Option<OffsetDateTime>,
+}
+
+impl MBC3 {
+    pub fn new(rom: &mut Rom, battery: bool) -> Result<Self> {
+        let bank_nb = rom.get_rom_size()? / 16;
+        let ram_size = rom.get_ram_size()?;
+        if ram_size != 0x0 && ram_size != 0x2000 && ram_size != 0x4000 && ram_size != 0x8000 {
+            bail!("Invalid ram size for MBC3: {:x}", ram_size);
+        }
+        let ram_bank_nb = ram_size / 0x2000;
+        let rom_data = rom.read_range(0, rom.get_data_len())?;
+        let mut save_path = rom.path.clone();
+        save_path.set_extension("save");
+        let mut save_file = if battery {
+            Some(
+                OpenOptions::new()
+                    .write(true)
+                    .read(true)
+                    .create(true)
+                    .open(&save_path)?,
+            )
+        } else {
+            None
+        };
+        let mut ram = vec![[0u8; 0x2000]; ram_bank_nb];
+        let mut time_offset = OffsetDateTime::UNIX_EPOCH - OffsetDateTime::now_utc();
+        let mut time_stopped = Some(OffsetDateTime::UNIX_EPOCH);
+        if let Some(save_file) = &mut save_file {
+            let file_len = save_file.seek(SeekFrom::End(0))?;
+            save_file.rewind()?;
+            if file_len != 0 {
+                for ram_bank in ram.iter_mut() {
+                    save_file.read_exact(ram_bank).with_context(|| {
+                        format!("Save file {} is corrupted", save_path.display())
+                    })?;
+                }
+                let mut time_bytes = [0u8; 8];
+                save_file.read_exact(&mut time_bytes).context("Failed to read save time info")?;
+                time_offset = Duration::new(i64::from_be_bytes(time_bytes), 0);
+                let mut time_stopped_byte = 0u8;
+                save_file.read_exact(slice::from_mut(&mut time_stopped_byte))?;
+                if time_stopped_byte != 0 {
+                    time_stopped = Some(OffsetDateTime::UNIX_EPOCH + time_offset);
+                } else {
+                    time_stopped = None
+                }
+                println!("Save file loaded from {}", save_path.display());
+                save_file.rewind()?;
+            } else {
+                println!("Save file created at {}", save_path.display());
+            }
+        }
+
+        let mut res = MBC3 {
+            banks: vec![[0; 0x4000]; bank_nb],
+            ram,
+            save_file,
+            ram_enable: false,
+            rom_selection: 1,
+            ram_is_rtc: false,
+            ram_rtc_selection: 0,
+            time_offset,
+            latched_time: None,
+            time_stopped
+        };
+        for (i, chunk) in rom_data.chunks(0x4000).enumerate() {
+            res.banks[i][..chunk.len()].copy_from_slice(chunk);
+        }
+        Ok(res)
+    }
+
+    fn get_game_time(&self) -> OffsetDateTime {
+        self.latched_time.or(self.time_stopped).unwrap_or_else(||OffsetDateTime::now_utc() + self.time_offset)
+    }
+
+    fn set_game_time(&mut self, new_time: &OffsetDateTime) {
+        if self.time_stopped.is_some() {
+            self.time_stopped = Some(*new_time);
+        } else {
+            eprintln!("Setting the clock while it is running");
+            self.time_offset = *new_time - OffsetDateTime::now_utc();
+        }
+    }
+}
+
+impl Drop for MBC3 {
+    fn drop(&mut self) {
+        if let Err(e) = (|| -> Result<()> {
+            if let Some(ram_file) = &mut self.save_file {
+                ram_file.rewind()?;
+                for ram_bank in self.ram.iter_mut() {
+                    ram_file.write_all(ram_bank)?;
+                }
+                if let Some(time_stopped) = self.time_stopped {
+                    ram_file.write_all(&(time_stopped - OffsetDateTime::UNIX_EPOCH).whole_seconds().to_be_bytes())?;
+                    ram_file.write_all(&[1u8])?;
+                } else {
+                    ram_file.write_all(&self.time_offset.whole_seconds().to_le_bytes())?;
+                    ram_file.write_all(&[0u8])?;
+                }
+                println!("Game saved!");
+            }
+            Ok(())
+        })() {
+            eprintln!("Game save failed: {e:#}");
+        }
+    }
+}
+
+impl Cartridge for MBC3 {
+    fn read(&self, addr: u16) -> u8 {
+        match addr {
+            x if x < 0x4000 => {
+                self.banks[0][addr as usize]
+            }
+            x if x < 0x8000 => self.banks[self.rom_selection as usize][addr as usize - 0x4000],
+            x if (0xa000..0xc000).contains(&x) => {
+                if !self.ram_enable {
+                    return 0;
+                }
+                if self.ram_is_rtc {
+                    match self.ram_rtc_selection {
+                        8 => self.get_game_time().second(),
+                        9 => self.get_game_time().minute(),
+                        10 => self.get_game_time().hour(),
+                        11 => ((self.get_game_time() - OffsetDateTime::UNIX_EPOCH).whole_days() & 0xff) as u8,
+                        12 => {
+                            let day_count = (self.get_game_time() - OffsetDateTime::UNIX_EPOCH).whole_days();
+                            let mut res = 0u8;
+                            if day_count & 0x100 != 0 {
+                                res |= 1;
+                            }
+                            if day_count>= 0x200 {
+                                res |= 0x80;
+                            }
+                            res |= if self.time_stopped.is_some() {0x40} else {0};
+                            res
+                        },
+                        _ => panic!("Invalid MBC3 RTC selection at {:x}", self.ram_rtc_selection)
+                    }
+                } else {
+                    self.ram[self.ram_rtc_selection as usize][(addr - 0xa000) as usize]
+                }
+            }
+            _ => panic!("Illegal cartridge read at {addr:#x}"),
+        }
+    }
+    fn write(&mut self, addr: u16, val: u8) {
+        match addr {
+            x if (0xa000..0xc000).contains(&x) => {
+                if self.ram_enable {
+                    if self.ram_is_rtc {
+                        match self.ram_rtc_selection {
+                            8 => {
+                                self.set_game_time(&self.get_game_time().replace_second(val).expect("Invalid second write to RTC"));
+                            },
+                            9 => self.set_game_time(&self.get_game_time().replace_minute(val).expect("Invalid minute write to RTC")),
+                            10 => self.set_game_time(&self.get_game_time().replace_hour(val).expect("Invalid hour write to RTC")),
+                            11 => {
+                                let old_offset = self.get_game_time() - OffsetDateTime::UNIX_EPOCH;
+                                let old_seconds = old_offset.whole_seconds() % (3600*24);
+                                let old_days = old_offset.whole_days();
+                                let new_offset = Duration::seconds(old_seconds) + Duration::days(val as i64 + (old_days & 0x100));
+                                self.set_game_time(&(OffsetDateTime::UNIX_EPOCH + new_offset));
+                            }
+                            12 => {
+                                let old_offset = self.get_game_time() - OffsetDateTime::UNIX_EPOCH;
+                                let old_seconds = old_offset.whole_seconds() % (3600*24);
+                                let old_days = old_offset.whole_days();
+                                let new_offset = Duration::seconds(old_seconds) + Duration::days((((val & 1) as i64) << 8) | (old_days & 0xff));
+                                self.set_game_time(&(OffsetDateTime::UNIX_EPOCH + new_offset));
+                                // TODO: ignore overflow flag at 0x80 for now
+                                if val & 0x40 != 0 {
+                                    if self.time_stopped.is_none() {
+                                        self.time_stopped = Some(self.get_game_time());
+                                    }
+                                } else if let Some(time_stopped) = self.time_stopped{
+                                    self.time_offset = time_stopped - OffsetDateTime::now_utc();
+                                    self.time_stopped = None;
+                                }
+                            },
+                            _ => panic!("Invalid MBC3 RTC selection at {:x}", self.ram_rtc_selection)
+                        };
+                    } else {
+                        let index = addr as usize - 0xa000;
+                        self.ram[self.ram_rtc_selection as usize][index] = val;
+                    }
+                }
+            }
+            x if x < 0x2000 => {
+                self.ram_enable = val & 0xf == 0xa;
+            }
+            x if x < 0x4000 => {
+                let value = val & 0x7f;
+                self.rom_selection = if value > 0 {value} else {1};
+            }
+            x if x < 0x6000 => {
+                match val {
+                    0..=3 => {
+                        self.ram_is_rtc = false;
+                        self.ram_rtc_selection = val;
+                    }
+                    8..=12 => {
+                        self.ram_is_rtc = true;
+                        self.ram_rtc_selection = val;
+                    }
+                    _ => {
+                        eprintln!("Invalid RAM RTC selection: {val:#x}");
+                    }
+                }
+            }
+            x if x < 0x8000 => {
+                if val == 0 {
+                    self.latched_time = None;
+                } else {
+                    self.latched_time = Some(self.get_game_time());
                 }
             }
             _ => {
